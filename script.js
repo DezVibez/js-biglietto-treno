91aa64af89a99e4a04b93535e5c8afdc5a7feb69
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-06-21 14:14:51 +0200

    Prezzo biglietto con condizioni  e stampa

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va stampato in pagina forma umana (con massimo due decimali
+L'output del prezzo finale va stampato in pagina forma umana (con massimo due decimali)
 */
 
 
@@ -17,3 +17,17 @@ console.log (destinazione + " Km")
 
 let prezzoBiglietto = destinazione * 0.21
 console.log (prezzoBiglietto + " €")
+
+if (età < 18) {
+    prezzoBiglietto = prezzoBiglietto - ((20/100)* prezzoBiglietto)
+    console.log (prezzoBiglietto + " €")
+    alert("Il prezzo del Suo biglietto sarà scontato del 20%")
+}
+
+if (età > 65) {
+    prezzoBiglietto = prezzoBiglietto - ((40/100)* prezzoBiglietto)
+    console.log (prezzoBiglietto + " €")
+    alert("Il prezzo del Suo biglietto sarà scontato del 40%")
+}
+
+document.getElementById("prezzo").innerText = "Il prezzo del Suo biglietto è di " + prezzoBiglietto + " €";
